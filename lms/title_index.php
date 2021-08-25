cd86ac4e55e20c1751d214325aedf34a9b09a1eb
andilemutono <87846146+andilemutono@users.noreply.github.com>
2021-08-25 11:44:39 +0200

    Iale institute learner management system
    Iale institute learner management system for post-schools and universities

---
+++ lms/title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin\images\iale_logo-removebg-preview.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">IALE INSTITUTE</p>
+							<h3>
+
+							<p>Online Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>IALE EXCELS:</p>
+												<p>Excellence Competence and Educational</p>
+												<p>Leadership in Skills training and Technology</p>
+								</div>		
+						</div>		
+				</div>
